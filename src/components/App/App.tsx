a26f74f362f4d0e07df132d8d9335e8cca26995f
Minh Dao <minh.dao@futurice.com>
2021-06-12 23:56:03 +0300

    Initial setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './App.module.scss';
+
+const App = (): React.ReactElement => {
+  return <div className={styles.component}></div>;
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import './index.scss';
+
+import App from './components/App/App'
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
